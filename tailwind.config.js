1a6c68631eed708e5f0fd2b23c6d3a81cf64a029
MOrtmann90 <mauro.ortmann@gmail.com>
2023-07-26 21:06:54 -0300

    First commit | Tailwind installed via NPM

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      mainCol: "#102531",
+      secCol: "#eaeaea",
+      accentCol: "#2b8f88"
+    }
+  },
+  plugins: []
+};
