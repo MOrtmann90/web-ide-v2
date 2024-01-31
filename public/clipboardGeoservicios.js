747824ee33267be03a061f1dfc2648b68fbb0ea6
MOrtmann90 <mauro.ortmann@gmail.com>
2024-01-31 15:15:27 -0300

    FIX on clipboardGeoservicios.js

--- public/clipboardGeoservicios.js
+++ public/clipboardGeoservicios.js
@@ -1,23 +1,35 @@
 // Clipboard geoservicios
 var copiarWms = document.getElementById("wms-btn");
+var copiarWfs = document.getElementById("wfs-btn");
 var geoserviciosAlert = document.getElementById("geoservicios-alert");
 
 function activarAlertaCopia() {
   geoserviciosAlert.classList.toggle("opacity-0");
   setTimeout(() => {
     geoserviciosAlert.classList.toggle("opacity-0");
-  }, "3000");
+  }, 3000);
 }
 
-copiarWms.addEventListener("click", function () {
+copiarWms.addEventListener("click", async function () {
   activarAlertaCopia();
-  navigator.clipboard.writeText("http://cipres-bk.tandil.gov.ar/geoserver/wms");
+  try {
+    await navigator.clipboard.writeText(
+      "http://cipres-bk.tandil.gov.ar/geoserver/wms"
+    );
+    console.log("Texto copiado al portapapeles");
+  } catch (error) {
+    console.error("Error al copiar texto al portapapeles: " + error);
+  }
 });
 
-var copiarWfs = document.getElementById("wfs-btn");
-
-copiarWfs.addEventListener("click", function () {
+copiarWfs.addEventListener("click", async function () {
   activarAlertaCopia();
-  navigator.clipboard.writeText("http://cipres-bk.tandil.gov.ar/geoserver/wfs");
+  try {
+    await navigator.clipboard.writeText(
+      "http://cipres-bk.tandil.gov.ar/geoserver/wfs"
+    );
+    console.log("Texto copiado al portapapeles");
+  } catch (error) {
+    console.error("Error al copiar texto al portapapeles: " + error);
+  }
 });
-
